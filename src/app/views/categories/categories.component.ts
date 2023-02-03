a1c06e287bd178b18c043e361560e57f6335aa8e
sosedvovan <drugst@yandex.ru>
2023-02-03 09:21:23 +0300

    Commit #6 в цикле выводим все категории в боковй части

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Category} from "../../model/Category";
+import {DataHandlerService} from "../../service/data-handler.service";
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css']
 })
-export class CategoriesComponent {
+export class CategoriesComponent implements OnInit{
+
+  categories: Category[];
+
+  constructor(private dataHandler: DataHandlerService) {
+    this.categories = this.dataHandler.getCategories();
+  }
+
+  //вызывается автоматически после инициализации компонента
+  ngOnInit(): void {
+    console.log(this.categories)
+  }
 
 }
